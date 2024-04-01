f768ecfc071a8235d44437b47cf3d550bd8a9aca
evanknep <knepx001@umn.edu>
2024-04-01 11:09:21 -0500

    models run

--- R/week10.R
+++ R/week10.R
@@ -6,50 +6,43 @@ library(haven) #I don't specifically recall using this package in lecture but I 
 
 
 # Data Import and Cleaning
-gss_tbl <- read_sav("../data/GSS2016.sav", user_na = FALSE) %>%
+gss_tbl <- read_sav("../data/GSS2016.sav", user_na = TRUE) %>%
   mutate_all(as.numeric) %>%
   filter(!is.na(MOSTHRS)) %>%
   mutate(MOSTHRS = as.numeric((MOSTHRS))) %>%
   select(-c(HRS1,HRS2)) %>%
   select(which(colMeans(is.na(.)) < 0.75))  #Iterating over each column and returning a sum of the NA values (treat each as =1), then divide by number of samples in the column. Retain column if value is < 0.75
 
-glimpse(gss_tbl)
 #Analysis
 ctrl <- trainControl(
-  method = "repeatedcv", 
-  number = 10,           
-  repeats = 5,           
-  p = 0.75,              
+  method = "cv", 
+  number = 10,
+  p = 0.75, 
+  verboseIter = TRUE,
   savePredictions = TRUE 
 )
 
 
-gss_tbl %>%
-  train(MOSTHRS ~ .,
-        tuneGrid = expand.grid(
-          alpha = 0:1,
-          lambda = seq(0.0001, 1, length = 20)
-        ),
-        method = "lm",
-        trControl = ctrl,
-        preProcess = "medianImpute" )
 
+# I had originally set it up with a tuneGrid like we had in datacamp, which was running fine, 
+# but after force closing R (realized I had made a mistake like 5 minutes into processing)
+# my tuneGrid now returns errors and prevents running
+# Errors are all along the lines of : the tuning parameter grid should have columns intercept, mtry, seq, etc.,
 
 train_models <- function(model, data) {
-  model <- train(as.formula(MOSTHRS ~ .), 
+  model <- train(MOSTHRS ~ ., 
                  data = data,
-                 method = model, 
+                 method = model,
                  trControl = ctrl,
                  preProcess = "medianImpute")
   return(model)
-}
+                 }
 
-train_models("glmnet", gss_tbl)
-
-models <- lapply(c("lm", "glmnet", "ranger", "xgboost"), train_models, data = gss_tbl)
+train_models("lm", gss_tbl)
+models <- lapply(c("lm", "glmnet", "ranger", "xgbTree"), train_models, data = gss_tbl)
 
 # Print model results
-
+summary(models)
 #Visualization
 gss_tbl %>%
   ggplot(aes(x=MOSTHRS)) +
